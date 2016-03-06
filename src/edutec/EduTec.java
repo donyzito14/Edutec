f90d29428aee57dd269b0b2d072f51362c891cc5
Alumno <Alumno@A201-4>
2016-03-06 16:40:44 -0500

    new commit

---
+++ src/edutec/EduTec.java
@@ -0,0 +1,24 @@
+
+package edutec;
+public class EduTec {
+
+    public EduTec() {
+    }
+    
+    public double promedio(int n1 , int n2){
+        return (n1+n2)/2.0d;
+        
+    }
+    public double promedio(int n1 , int n2 , int n3){
+        return (n1+n2+n3)/3.0d;
+        
+    }
+    public double promedio(int n1 , int n2,int n3,int n4){
+        return (n1+n2+n3+n4)/4.0d;
+        
+    }
+    public double promedio(int n1 , int n2 , int n3 , int n4 , int n5){
+        return (n1+n2+n3+n4+n5)/5.0d;
+        
+    }
+}
